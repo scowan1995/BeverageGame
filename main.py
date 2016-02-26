bf75e4eb04cc8586982a8a4f91f9a4d728b493ff
Killian Davitt <davittk@tcd.ie>
2016-02-26 21:45:23 +0000

    Reorganised program structure

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from time import sleep as shlep
 from random import randint as alan
 
-
-
-
-
 class Player:
     def __init__(self, name):
         self.name=name
@@ -12,30 +8,19 @@ class Player:
 
     def getName(self):
         return self.name
-
 capitals = []
-# import capitals
-with open('capitals.txt', 'r') as f:
-    p = f.read().splitlines()
-    capitals = [x.split(',') for x in p]
-
-
-num = input("Enter number of players:\n")
-players = []
-num = int(num)
-while num>0:
-    name = input("Enter a player name:")
-    new_player = Player(name)
-    players.append(new_player)
-    num=num-1
-
+    
 
 def capital_round(n, players):
     print("\n\n -------------ROUND %d----------\n %s:\n" %(n, "Random"))
-    
+    # import capitals
+    with open('capitals.txt', 'r') as f:
+        p = f.read().splitlines()
+        capitals = [x.split(',') for x in p]
+
     for player in players:
         print(player.getName() + " YOUR TURN")
-        capital = capitals[alan(0, len(capitals))]
+        capital = capitals[alan(0, len(capitals)-1)]
         answer = input("What is the capital of " +  capital[0])
         if answer==capital[1]:
             print("Correct! Pick a person to drink!")
@@ -51,15 +36,27 @@ def random(n, players):
         i=i-1
 
 
+def main():
+    num = input("Enter number of players:\n")
+    players = []
+    num = int(num)
+    while num>0:
+        name = input("Enter a player name:")
+        new_player = Player(name)
+        players.append(new_player)
+        num=num-1
 
-rounds = [capital_round, random]
-n=0
-while True:
-    curr_round_num=alan(0,len(rounds))
-    curr_round=rounds[curr_round_num]
-    n=n+1
-    curr_round(n,players)
+    rounds = [capital_round, random]
+    n=0
+    while True:
+        curr_round_num=alan(0,len(rounds)-1)
+        curr_round=rounds[curr_round_num]
+        n=n+1
+        curr_round(n,players)
 
+
+if __name__=="__main__":
+    main()
 
 
 
