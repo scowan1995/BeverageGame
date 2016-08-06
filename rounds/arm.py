89d8c833228e61657dd4232fe1f9dff914d97b73
Sean Cowan <scowan@scss.tcd.ie>
2016-08-06 18:45:42 +0100

    modified arms so that a player does not end up arm wrestling themselves

--- rounds/arm.py
+++ rounds/arm.py
@@ -1,6 +1,10 @@
 import time
 
 def arm(players):
-    print(players.getRandomPlayerName() + " must arm wrestle " + players.getRandomPlayerName())
+    p1 = players.getRandomPlayerName()
+    p2 = p1
+    while p1 == p2:
+        p2 = players.getRandomPlayerName()
+    print(p1 + " must arm wrestle " + p2)
     time.sleep(10)
     input()
